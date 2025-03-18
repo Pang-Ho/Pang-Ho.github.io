ba813dd6b168785c4bcf50ee3752b3ea317c13f4
Pang <slsl01@naver.com>
2025-03-18 10:53:16 +0900

    Feat : SEO 이미지를 Favicon으로 변경

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -14,7 +14,7 @@ const SEO = ({ title, description, url }) => {
       )}
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
-      <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta property="og:image" content={`${siteUrl}/favicon.png`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
